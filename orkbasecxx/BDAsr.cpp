b94f90e561c32bfb9b4f5bed77f5ce7d5eec45fa
wwz <1823553809@qq.com>
2020-01-19 16:06:40 +0800

    asr: add baidu asr
    Signed-off-by: wwz <1823553809@qq.com>

---
+++ orkbasecxx/BDAsr.cpp
@@ -0,0 +1,117 @@
+#include "BDAsr.h"
+#include "LogManager.h"
+#include "ConfigManager.h"
+
+
+void default_callback(AudioFragmentResponse& resp, 
+                      void* data) {
+    std::stringstream ss;
+    if (data) {
+        char* tmp = (char*) data;
+        CStdString logMsg;
+        logMsg.Format(" recived msg %s", tmp);
+        LOG4CXX_INFO(LOG.asrLog, logMsg);
+    }
+    if (resp.type() == com::baidu::acu::pie::FRAGMENT_DATA) {
+        AudioFragmentResult *audio_fragment = resp.mutable_audio_fragment();
+        ss << "Receive " << (audio_fragment->completed() ? "completed" : "uncompleted")
+           << ", serial_num=" << audio_fragment->serial_num()
+           << ", start=" << audio_fragment->start_time()
+           << ", end=" << audio_fragment->end_time()
+           << ", error_code=" << resp.error_code()
+           << ", error_message=" << resp.error_message()
+           << ", content=" << audio_fragment->result();
+        std::cout << ss.str() << std::endl;
+        LOG4CXX_INFO(LOG.asrLog, ss.str());
+    } else {
+        std::stringstream ss;
+        ss << "error resp type is=" << resp.type();
+        LOG4CXX_INFO(LOG.asrLog, ss.str());
+       
+    }
+}
+
+
+
+void write_to_stream(AsrClient client, AsrStream* stream,
+                    char* buffer, int i) {
+    int size = client.get_send_package_size();
+    size_t count = 0;
+    while (i > 0) {
+        if (i > size) {
+            count = size;
+        }
+        else {
+            count = i;
+        }
+        if (stream->write(buffer, count, false) != 0) {
+            LOG4CXX_ERROR(LOG.asrLog, "[error] stream write buffer error");
+            break;
+        }
+        usleep(20*1000);
+        if (i < size) {
+            LOG4CXX_INFO(LOG.asrLog, " skip write_to_stream loop");
+            break;
+        }
+        else {
+            i = i - size;
+        }
+        buffer = buffer + count;
+    }
+    LOG4CXX_INFO(LOG.asrLog, " write stream to asr , end");
+    stream->write(nullptr, 0, true);
+}
+
+void CAsrPortal::init_asr() {
+  portal = CONFIG.m_asr_portal;
+  user = CONFIG.m_asr_product_id;
+  pwd = CONFIG.m_asr_user;
+  pid = CONFIG.m_asr_pwd;
+}
+
+
+bool CAsrPortal::connect_asr_server(CStdString expire_time) {
+  client.set_app_name("cpp_client");
+  client.set_enable_flush_data(true);
+  client.set_product_id(pid);
+  client.init(portal, 0);
+  client.set_user_name(user);
+  std::string passwd = pwd;
+  std::string str = user + pwd + expire_time;
+  std::string token = picosha2::hash256_hex_string(str);
+  client.set_token(token);
+  client.set_expire_time(expire_time); //expire_time UTC format, 2019-04-25T12:41:16Z
+  stream = client.get_stream();
+  if (stream) {
+    LOG4CXX_INFO(LOG.asrLog, " client get stream success"); 
+  } 
+  else {
+    return false;
+  }
+  return true;
+}
+
+
+void CAsrPortal::send_voice_stream(char* buffer, int i) {
+  std::thread writer(write_to_stream, client, stream, buffer, i);
+  writer.detach();   
+
+  int read_num = 1;
+  char tmp[100] = "\0";
+  while(1) {
+    if (stream->read(default_callback, tmp) != 0) {
+      break;
+    }
+    std::stringstream ss;
+    ss << "[debug] read stream return " 
+                      << read_num++ << "times";
+    LOG4CXX_INFO(LOG.asrLog, ss.str())
+  }
+  uninit_asr();  
+}
+
+void CAsrPortal::uninit_asr() {
+  client.destroy_stream(stream);
+}
+
+
---
+++ orkbasecxx/BDAsr.h
@@ -0,0 +1,39 @@
+#ifndef _ASR_PORTAL_
+#define _ASR_PORTAL_
+
+#include "asr_bd/audio_streaming_client.h"
+#include "asr_bd/picosha2.h"
+#include "Utils.h"
+#include "shared_ptr.h"
+
+typedef com::baidu::acu::pie::AsrClient AsrClient;
+typedef com::baidu::acu::pie::AsrStream AsrStream;
+typedef com::baidu::acu::pie::AudioFragmentResponse AudioFragmentResponse;
+typedef com::baidu::acu::pie::AudioFragmentResult AudioFragmentResult;
+
+
+class  CAsrPortal;
+typedef oreka::shared_ptr<CAsrPortal> CAsrPortalRef;
+
+
+class CAsrPortal {
+
+  private:
+    CStdString portal;
+    CStdString user;
+    CStdString pwd;
+    CStdString pid;
+    AsrClient client;
+    AsrStream* stream {nullptr};
+  public:
+    void init_asr();
+    bool connect_asr_server(CStdString expire_time);
+    void send_voice_stream(char* buffer, int i);
+    bool get_result(); 
+    void uninit_asr();
+
+
+};
+
+#endif
+
